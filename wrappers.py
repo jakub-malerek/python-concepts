977862613e08781e04960f9b0125e97f513552f9
Jakub <jakubmalerek@gmail.com>
2024-03-07 09:28:11 +0100

    wrappers.py

--- wrappers.py
+++ wrappers.py
@@ -5,10 +5,48 @@ def decorator_func(func):
         print("after")
     return wrapper  
 
-@decorator_func
-def func():
-    print("inner func")
+
+def func(a,b):
+    print("inner func",a,b)
 
 
-func()
+def argument_decorator_func(func):
+    def wrapper(*args,**kwargs):
+        print("before")
+        result = func(*args,**kwargs)
+        print("after")
+        return result
+    return wrapper
+
+func = argument_decorator_func(func)
+func(10,20)
+
+import time
+
+
+def time_threshold(threshold):
+    def decorator(func):
+        def wrapper():
+            start = time.time()
+            result = func()
+            end = time.time()
+            if (end-start) > threshold:
+                 print(f"{func.__name__} took {(end-start)} seconds to execute, which exceeds the threshold.")
+        return wrapper
+    return decorator
+
+
+
+
+    
+
+
+
+@time_threshold(threshold=0.5)
+def long_running_function():
+    # Simulate a time-consuming operation
+    time.sleep(1)  # Sleep for 1 second
+    print("Function execution completed.")
+
+long_running_function()
 
